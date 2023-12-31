8af3aeb2aa60b676f2386d13cbb15875c9252ee3
gazimdparveg <storybook133@gmail.com>
2024-01-01 02:38:17 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Pagese/Home';
+import About from './Pagese/About';
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+       <Home/>
+       <About/>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
